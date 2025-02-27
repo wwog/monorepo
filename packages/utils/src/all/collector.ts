223a84e9cec8f7340d62888c07fc603c58f58935
wwog <871782513@qq.com>
2025-02-27 14:38:23 +0800

    -

--- packages/utils/src/all/collector.ts
+++ packages/utils/src/all/collector.ts
@@ -1,7 +1,9 @@
 //#region Collector
-type BatchFunction<Args extends any[], Result> = (argsList: Args[]) => Result
+export type BatchFunction<Args extends any[], Result> = (
+  argsList: Args[],
+) => Result
 
-interface CollectorOptions<Args extends any[], Result = any> {
+export interface CollectorOptions<Args extends any[], Result = any> {
   /**
    * @description 最大收集数量
    * @description_en Maximum number of collections
@@ -12,6 +14,11 @@ interface CollectorOptions<Args extends any[], Result = any> {
    * @description_en Timeout to automatically execute (milliseconds)
    */
   waitMs?: number
+  /**
+   * @description 自动执行,开启后您必须传递waitMs或者maxCount(否则报错),默认为true. 当你想要手动调用时,可以设置为false
+   * @description_en Automatically execute, must pass waitMs or maxCount (otherwise an error will be reported), the default is true. When you want to manually call it, you can set it to false
+   */
+  autoFlush?: boolean
   /**
    * @description 执行前回调，可以通过返回false来阻止执行
    * @description_en Callback before execution, can return false to prevent execution
@@ -29,7 +36,8 @@ interface CollectorOptions<Args extends any[], Result = any> {
   onError?: (error: Error, argsList: Args[]) => void | Promise<void>
 }
 
-interface CollectedFunction<Args extends any[], Result = any> extends Function {
+export interface CollectedFunction<Args extends any[], Result = any>
+  extends Function {
   (...args: Args): void
   /**
    * @description 手动执行,满足条件的执行会返回结果,否则会返回undefined
@@ -43,22 +51,60 @@ interface CollectedFunction<Args extends any[], Result = any> extends Function {
   updateOptions(options: Partial<CollectorOptions<Args>>): void
 }
 
-function createCollector<Args extends any[], Result = any>(
+/**
+ * @description 创建一个收集器
+ * @description_en Create a collector
+ * @example
+ * ```ts
+ * // manual mode
+ * const collector = createCollector((argsList) => {
+ *   return argsList.map((args) => {
+ *     return args.reduce((acc, curr) => acc + curr, 0)
+ *   })
+ * })
+ * collector(1, 2, 3)
+ * collector(4, 5, 6)
+ * collector(7, 8, 9)
+ * collector.flush() // [6, 15, 24]
+ * ```
+ * ```ts
+ * // auto mode
+ * const collector = createCollector((argsList) => {
+ *   return argsList.map((args) => {
+ *     return args.reduce((acc, curr) => acc + curr, 0)
+ *   })
+ * }, {
+ *   autoFlush: true,
+ *   waitMs: 1000,
+ * })
+ * collector(1, 2, 3)
+ * collector(4, 5, 6)
+ */
+export function createCollector<Args extends any[], Result = any>(
   batchFn: BatchFunction<Args, Result>,
   options?: CollectorOptions<Args, Result>,
 ) {
-  if (options?.waitMs === undefined && options?.maxCount === undefined) {
-    throw new Error('waitMs or maxCount must be provided')
+  const autoFlush = options?.autoFlush ?? true
+  const checkOptions = () => {
+    if (autoFlush === false) {
+      return
+    }
+    if (options?.waitMs === undefined && options?.maxCount === undefined) {
+      throw new Error('waitMs or maxCount must be provided')
+    }
+    if (
+      options?.waitMs !== undefined &&
+      (options?.waitMs <= 0 || options?.waitMs === Infinity)
+    ) {
+      throw new Error('waitMs must be a positive number')
+    }
+    if (options?.maxCount !== undefined && options?.maxCount <= 0) {
+      throw new Error('maxCount must be a positive number')
+    }
   }
-  if (
-    options?.waitMs !== undefined &&
-    (options?.waitMs <= 0 || options?.waitMs === Infinity)
-  ) {
-    throw new Error('waitMs must be a positive number')
-  }
-  if (options?.maxCount !== undefined && options?.maxCount <= 0) {
-    throw new Error('maxCount must be a positive number')
-  }
+
+  checkOptions()
+
   let currentOptions: CollectorOptions<Args, Result> = {
     ...options,
   }
